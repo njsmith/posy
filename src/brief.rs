bd5dfbf67fe72cd177ba5bd9fec3a73e3f39513d
Nathaniel J. Smith <njs@pobox.com>
2022-06-19 22:52:19 -0700

    starting to sketch out pinning/resolution types

--- src/brief.rs
+++ src/brief.rs
@@ -2,6 +2,9 @@ use crate::prelude::*;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum PinPlatform {
+    Explicit {
+        markers: HashMap<String, String>,
+    },
     Local,
 }
 
@@ -14,13 +17,24 @@ impl PinPlatform {
         index: super::package_index::PackageIndex,
     ) -> Result<HashMap<String, String>> {
         // some options:
-        // - find a pybi match the given settings and get its markers
+        // - find a pybi matching the given settings and get its markers
         // - calculate markers directly for current system (possibly based on some
         //   explicit template), or even return a canned response
         // - build a python locally and then query...
-        todo!()
+        Ok(match self {
+            PinPlatform::Explicit { markers } => markers.clone(),
+            PinPlatform::Local => todo!(),
+        })
     }
 }
+
+// blueprint:
+// - exactly one pinned pybi
+// - list of marker predicates + value that were relied on to generate this, so can
+//   generalize to other pythons (not used for now, could defer?)
+// - exact list of package+version(+url for @ dependencies?)
+// - hashes
+
 
 /// A high-level description of an environment that a user would like to be able to
 /// build. Doesn't necessarily have to be what the user types in exactly, but has to
@@ -39,11 +53,14 @@ pub struct Brief {
     // and we'll see how far we get with that + diagnostic hints for when the user
     // should add a package here to make things work.
     pub allow_pre: HashSet<PackageName>,
+    pub allow_pre_all: bool,
     pub platforms: Vec<PinPlatform>,
 }
 // should these all be Cow's? if we want to generalize allow_pre to functions (e.g. so
 // can do "resolve but with allow_pre=|pkg| {true}"!), then we'll want to be able to
 // borrow closures...
+// meh for now we'll just leave it hardcoded, and adjust if needed. This also makes
+// Debug/Serialize way easier.
 
 impl Default for Brief {
     fn default() -> Self {
@@ -76,26 +93,24 @@ impl Brief {
     }
 }
 
+struct PackagePin
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Blueprint {
-    // the brief we were built from
-// (except maybe not the platform requests?)
-// (eh, I guess we should save them here for "is the blueprint stale" check, even if we
-// don't use them otherwise)
+    // set of package pins
+    // links to artifacts + hashes
+    // dependencies we used to compute the pins + provenance
+    // split up by platform / marker tags
+}
 
-// set of package pins
-// links to artifacts + hashes
-// dependencies we used to compute the pins + provenance
-// split up by platform / marker tags
+pub trait PyEnvMaker {
+    fn make(&self, blueprint: &Blueprint) -> Result<PyEnv>;
 }
 
 pub struct PyEnv {
-    // for regular on-disk envs, need to be able to query to get:
-//   markers
-//   tags
-//   installed packages
-// and then renovate to remove existing packages, add new ones
-// ideally in parallel, while handling file conflicts gracefully
+    pub envvars: HashMap<String, String>,
+    // destructor? not necessary in short term, but in long term might want to track
+    // references so can do GC
 }
 
 pub struct ProjectWorkspace {}
@@ -114,12 +129,6 @@ impl ProjectWorkspace {
 // manipulation + share copies of python/packages, including concurrently?
 impl TempWorkspace {
     pub fn get_env(blueprint: &Blueprint) -> PyEnv {
-        todo!()
-    }
-}
-
-impl PyEnv {
-    pub fn run() {
         todo!()
     }
 }
