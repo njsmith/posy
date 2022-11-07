07b9475f2f363fc5869843a3ad49a385f18fac8f
Nathaniel J. Smith <njs@pobox.com>
2022-11-06 19:21:59 -0800

    Add Windows script trampolines

---
+++ src/windows-trampolines/posy-trampoline/src/bin/posy-trampoline-gui.rs
@@ -0,0 +1,10 @@
+#![feature(default_alloc_error_handler)]
+#![no_std]
+#![no_main]
+#![windows_subsystem = "windows"]
+
+// build.rs passes a custom linker flag to make this the entrypoint to the executable
+#[no_mangle]
+pub extern "C" fn entry() -> ! {
+    posy_trampoline::bounce::bounce(true)
+}
---
+++ src/windows-trampolines/posy-trampoline/src/lib.rs
@@ -0,0 +1,7 @@
+#![feature(panic_info_message)]
+#![no_std]
+
+mod runtime;
+mod helpers;
+mod diagnostics;
+pub mod bounce;
